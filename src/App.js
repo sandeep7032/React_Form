70b06839488f57e590d7f34952bec8055c9d8ef4
Sandeep <95558185+sandeep7032@users.noreply.github.com>
2023-07-07 10:40:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <b><h2 style={{ margin: "30px", fontSize: "larger" }}>Registration Form</h2></b>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
